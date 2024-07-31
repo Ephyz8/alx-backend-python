448968b5b79b42d2ee16be2054e4f34dcce3c90c
Ephraim <135040822+Ephyz8@users.noreply.github.com>
2024-07-31 19:21:15 +0200

    testing

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -14,7 +14,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     Calculate the start and end index for pagination.
     
     Args:
-    - page (int): The current page number (1-indexed).
+    - page (int): The current page number (1-indexed)
     - page_size (int): The number of items per page.
     
     Returns:
