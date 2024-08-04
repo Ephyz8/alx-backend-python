4297b4259b4d6c8df859c180d103ac5e8cf582a9
Ephyz8 <ziwoyaephraim@gmail.com>
2024-08-04 15:42:27 +0000

    LIFO

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
-"""Task 2: LIFO Caching.
-"""
-from collections import OrderedDict
+""" LIFOCache module """
 
 from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """Represents an object that allows storing and
-    retrieving items from a dictionary with a LIFO
-    removal mechanism when the limit is reached.
     """
+    LIFOCache class is a caching system that uses the LIFO (Last-In, First-Out)
+    algorithm.
+    """
+
     def __init__(self):
-        """Initializes the cache
-        """
+        """Initialize the cache and order tracking list."""
         super().__init__()
         self.cache_data = OrderedDict()
 
@@ -23,12 +21,11 @@ class LIFOCache(BaseCaching):
 
         Args:
             key: The key associated with the item.
-            item: The item to be cached.
+            item: The item to be cached
 
         If key or item is None, this method does nothing.
         If the number of items in self.cache_data exceeds MAX_ITEMS, the last
         item put in cache is discarded following the LIFO algorithm.
-        """Adds an item in the cache.
         """
         if key is None or item is None:
             return
@@ -41,15 +38,10 @@ class LIFOCache(BaseCaching):
             print("DISCARD:", last_key)
 
         # Add or update the item in the cache
-        if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                last_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", last_key)
         self.cache_data[key] = item
 
         # Move the key to the end to signify it as the most recently used
         self.cache_data.move_to_end(key)
-        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
         """Retrieve an item by key from the cache.
@@ -60,7 +52,5 @@ class LIFOCache(BaseCaching):
         Returns:
             The item associated with the key, or None if the key is None
             or if the key doesn't exist in the cache.
-        """Retrieves an item by key.
         """
-        return self.cache_data.get(key, None)
-    
+        return self.cache_data.get(key)
