c49b3786e9fddd6d1450c74977af0e2c3d53dbd1
Ephraim <135040822+Ephyz8@users.noreply.github.com>
2024-08-04 10:06:51 +0000

    configs

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 """ LIFOCache module """
 
+from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ LIFOCache class caching system: Last-In, First-Out algorithm. """
+    """LIFOCache class caching system: (Last-In, First-Out) algorithm."""
 
     def __init__(self):
-        """ Initialize the cache and order tracking list. """
+        """Initialize the cache and order tracking list."""
         super().__init__()
-        self.keys_order = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item in the cache.
+        """Add an item in the cache.
 
         Args:
             key: The key associated with the item.
@@ -21,22 +22,25 @@ class LIFOCache(BaseCaching):
 
         If key or item is None, this method does nothing.
         If the number of items in self.cache_data exceeds MAX_ITEMS, the last
-        item put in cache is discarded following LIFO algorithm.
+        item put in cache is discarded following the LIFO algorithm.
         """
         if key is None or item is None:
             return
 
-        if key not in self.cache_data:
-            self.keys_order.append(key)
+        # If the key is not already in cache_data, and adding it will exceed MAX_ITEMS, evict the last item
+        if key not in self.cache_data and len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # Pop the last item (most recently added) in LIFO manner
+            last_key, _ = self.cache_data.popitem(last=True)
+            print("DISCARD:", last_key)
+
+        # Add or update the item in the cache
         self.cache_data[key] = item
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            last_key = self.keys_order.pop()
-            del self.cache_data[last_key]
-            print(f"DISCARD: {last_key}")
+        # Move the key to the end to signify it as the most recently used
+        self.cache_data.move_to_end(key)
 
     def get(self, key):
-        """ Retrieve an item by key from the cache.
+        """Retrieve an item by key from the cache.
 
         Args:
             key: The key for the item to retrieve.
